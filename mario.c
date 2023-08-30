5b3a1fd375b8f2e09b9a75cdfeb174d5f242c571
xiu-yuhan <140463453+xiu-yuhan@users.noreply.github.com>
2023-08-30 15:36:14 -0400

    Add files via upload

--- mario.c
+++ mario.c
@@ -3,18 +3,24 @@
 
 int main(void)
 {
+    //get height
     int height = 0;
     while (height < 1 | height > 8){
         height = get_int("Height: ");
     }
+    //loop to make every layer of pyramid
     for (int i = 1; i < height+1; i++){
+        //creates spaces before pyramid
         for (int s = height-i; s >0 ; s--){
             printf(" ");
         }
+        //creates left side of pyramid
         for (int b = i; b > 0; b--){
             printf("#");
         }
+        //creates hole in middle
         printf("  ");
+        //creates right side of pyramid
         for (int c = i; c > 0; c--){
             printf("#");
         }
